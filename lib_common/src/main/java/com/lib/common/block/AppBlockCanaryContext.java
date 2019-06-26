aa78dac5c3bcc300c8dd09c139b79c401eef4848
yellowbaby <13660455640@163.com>
2019-06-26 10:01:02 +0800

    修复blockcannary

--- lib_common/src/main/java/com/lib/common/block/AppBlockCanaryContext.java
+++ lib_common/src/main/java/com/lib/common/block/AppBlockCanaryContext.java
@@ -1,23 +1,23 @@
 package com.lib.common.block;
 
 
+import android.content.pm.PackageInfo;
+
 import com.github.moduth.blockcanary.BlockCanaryContext;
 import com.github.moduth.blockcanary.BuildConfig;
 
 public class AppBlockCanaryContext extends BlockCanaryContext {
 
     //设置卡顿判断的阙值
-    public int getConfigBlockThreshold() {
+    public int provideBlockThreshold() {
         return 500;
     }
 
     //是否需要显示卡顿的信息
-    public boolean isNeedDisplay() {
+    public boolean displayNotification() {
         return BuildConfig.DEBUG;
     }
 
-    //设置log保存在sd卡的目录位置
-    public String getLogPath() {
-        return "/blockcanary/performance";
-    }
+
+
 }
