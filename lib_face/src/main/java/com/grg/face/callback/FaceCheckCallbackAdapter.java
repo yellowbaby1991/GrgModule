224cc4f71fa48ca8d69ee52a366503cf362ac03f
yellowbaby <13660455640@163.com>
2019-07-05 11:36:45 +0800

    新增adpter

---
+++ lib_face/src/main/java/com/grg/face/callback/FaceCheckCallbackAdapter.java
@@ -0,0 +1,17 @@
+package com.grg.face.callback;
+
+import android.graphics.Bitmap;
+import android.graphics.RectF;
+
+public abstract class FaceCheckCallbackAdapter implements FaceCheckCallback{
+
+    @Override
+    public void loseFace() {
+
+    }
+
+    @Override
+    public void getFaceLocation(RectF faceRect) {
+
+    }
+}
