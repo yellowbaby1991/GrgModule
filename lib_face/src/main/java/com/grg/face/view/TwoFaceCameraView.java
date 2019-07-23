7bc3dceeb207507165e18ffb5451a0ec99ce7eb9
yellowbaby <13660455640@163.com>
2019-07-23 15:38:53 +0800

    提交

--- lib_face/src/main/java/com/grg/face/view/TwoFaceCameraView.java
+++ lib_face/src/main/java/com/grg/face/view/TwoFaceCameraView.java
@@ -39,6 +39,7 @@ public class TwoFaceCameraView extends BaseCameraView{
     @Override
     protected void openTwoCamera(int cameraID1, int cameraID2, int width, int height) {
         setOpenIgore(true);//单目非活体的时候过滤前两帧废数据
+        setMAXIngoreNum(4);
         super.openTwoCamera(cameraID1,cameraID2,width,height);
         mCameraVtv1.startPreview(cameraID1, width, height);
         mCameraVtv2.startPreview(cameraID2, width, height);
